240d6af5d8d5507e885548cd22034b9cd11374a2
pratikand7 <pratik.aneelam.97@gmail.com>
2023-11-08 14:26:15 +0530

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require('./routes/tasks')
 const connectDb = require('./db/connect')
+require('dotenv').config()
 
 app.use(express.json())
 
@@ -15,7 +16,7 @@ const port = 3000;
 
 const start = async()=>{
     try {
-        await connectDb()
+        await connectDb(process.env.MONGO_URI)
         app.listen(port, console.log(`server is running on port ${port} ...`));
     } catch (error) {
         console.log(error)
--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const connection = "mongodb+srv://pratikuymar:Sigma1234@cluster0.lcssvsk.mongodb.net/task-manager?retryWrites=true&w=majority";
-
 const connectDb = (url) => {
-    return mongoose
-    .connect(connection, {
+    return mongoose.connect(url, {
         useNewUrlParser:true,
         useUnifiedTopology: true,
     })
