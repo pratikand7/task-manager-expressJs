5d7b3afd3639121c6eb41b0a7d6b750ee3f13693
pratikand7 <pratik.aneelam.97@gmail.com>
2023-11-08 12:53:37 +0530

    commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('./db/connect')
+
 const express = require("express");
 
 const app = express();
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,8 @@
-const connection = "mongodb+srv://pratikuymar:<password>@cluster0.lcssvsk.mongodb.net/"
+const mongoose = require("mongoose");
+
+const connection = "mongodb+srv://pratikuymar:Sigma1234@cluster0.lcssvsk.mongodb.net/task-manager?retryWrites=true&w=majority";
+
+mongoose
+    .connect(connection)
+    .then(()=>console.log('connected to the db...'))
+    .catch((err)=>console.log(err));
